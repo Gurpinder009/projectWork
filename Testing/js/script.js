56b4ccb53fff0bb627be479d21137993041d541d
Gurpinder09 <grewalgurpinder09@gmail.com>
2022-02-26 06:41:22 +0530

    slider installed

--- Testing/js/script.js
+++ Testing/js/script.js
@@ -1,4 +1,13 @@
 function menu(){
-    let x = document.getElementById('navbar');
-    (x.style.height !== "9em")?x.style.height = "9em":x.style.height="4.5em";
+    let x = document.documentElement.style;
+    if( document.querySelector("#drawer").offsetWidth !==0  ){
+        x.setProperty("--width-drawer","0em");
+        document.getElementById("menu").style.display = "inline-block";
+
+    }else{
+        document.getElementById("menu").style.display = "none";
+        x.setProperty("--width-drawer","15em");
+    }
+
+  
 }
